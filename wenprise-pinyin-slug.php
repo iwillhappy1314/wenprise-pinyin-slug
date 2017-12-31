c4e5d7d6392bb5cbfc0448ee5b60bf2875b4da11
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2017-12-31 20:50:14 +0800

    修改 plugin url

--- wenprise-pinyin-slug.php
+++ wenprise-pinyin-slug.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name:        Wenprise Pinyin Slug
-Plugin URI:         https://www.wpzhiku.com/
+Plugin URI:         https://www.wpzhiku.com/wenprise-pinyin-slug/
 Description:        自动转换 WordPress 中的中文文章别名、分类项目别名、图片文件名称为汉语拼音。
 Version:            1.0
 Author:             Amos Lee
