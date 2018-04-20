5377e2fdb706ad99cc524f087b880383ff173c28
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2018-04-20 17:02:22 +0800

    解决更新分类时，别名不能转拼音的 Bug

--- src/slug.php
+++ src/slug.php
@@ -13,7 +13,7 @@ add_filter( 'name_save_pre', function ( $slug ) {
 	$title = wprs_slug_convert( $_POST[ 'post_title' ] );
 
 	return sanitize_title( $title );
-}, 0 );
+}, 10, 1 );
 
 
 /**
@@ -31,10 +31,52 @@ add_filter( 'pre_category_nicename', function ( $slug ) {
 	}
 
 	// 替换文章标题
-	$title = wprs_slug_convert( $_POST[ 'tag-name' ] );
+	$slug = wprs_slug_convert( $_POST[ 'tag-name' ] );
 
-	return sanitize_title( $title );
-}, 0 );
+	return sanitize_title( $slug );
+}, 10, 1 );
+
+
+/**
+ * 添加分类时替换分类标题为拼音
+ *
+ * @param $data     array 需要保存到数据库中的数据
+ * @param $term_id  int 分类项目 ID
+ * @param $taxonomy string 分类法名称
+ * @param $args     array 用户提交的数据
+ *
+ * @return array 修改后的需要保存到数据库中的数据
+ */
+add_filter( 'wp_insert_term_data', function ( $data, $taxonomy, $args ) {
+
+	// 手动编辑时，不自动转换为拼音
+	if ( $args[ 'slug' ] === '' ) {
+		$data[ 'slug' ] = wp_unique_term_slug( wprs_slug_convert( $data[ 'name' ] ), (object) $args );
+	}
+
+	return $data;
+}, 10, 3 );
+
+
+/**
+ * 更新分类时分类标题为拼音
+ *
+ * @param $data     array 需要保存到数据库中的数据
+ * @param $term_id  int 分类项目 ID
+ * @param $taxonomy string 分类法名称
+ * @param $args     array 用户提交的数据
+ *
+ * @return array 修改后的需要保存到数据库中的数据
+ */
+add_filter( 'wp_update_term_data', function ( $data, $term_id, $taxonomy, $args ) {
+
+	// 手动编辑时，不自动转换为拼音
+	if ( $args[ 'slug' ] === '' ) {
+		$data[ 'slug' ] = wp_unique_term_slug( wprs_slug_convert( $data[ 'name' ] ), (object) $args );
+	}
+
+	return $data;
+}, 10, 4 );
 
 
 /**
@@ -63,7 +105,7 @@ add_filter( 'sanitize_file_name', function ( $filename ) {
 
 	return $filename;
 
-}, 0 );
+}, 10, 1 );
 
 
 /**
@@ -74,7 +116,8 @@ add_filter( 'sanitize_file_name', function ( $filename ) {
  * @return string 转换后的拼音
  */
 if ( ! function_exists( 'wprs_slug_convert' ) ) {
-	function wprs_slug_convert( $name ) {
+	function wprs_slug_convert( $name )
+	{
 
 		$divider = wprs_plugin_get_option( 'wprs_pinyin_slug', 'divider', '-' );
 		$type    = wprs_plugin_get_option( 'wprs_pinyin_slug', 'type', '0' );
