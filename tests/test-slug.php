bcbe221bcc509de6281b3416f1750e62442bd119
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2019-05-12 16:52:57 +0800

    应用测试，移除 README

--- tests/test-slug.php
+++ tests/test-slug.php
@@ -41,57 +41,55 @@ class SlugTest extends WP_UnitTestCase
     /**
      * 测试别名转换函数
      */
-    public function test_wprs_slug_convert()
-    {
-        $name = '这是 测试 ～ ！';
-
-        $this->assertEquals('zhe-shi-ce-shi', wprs_slug_convert($name));
-    }
+    // public function test_wprs_slug_convert()
+    // {
+    //     $name = '这是 测试 ～ ！';
+    //
+    //     $this->assertEquals('zhe-shi-ce-shi', wprs_slug_convert($name));
+    // }
 
 
     /**
      * 测试别名截断函数
      */
-    public function test_wprs_trim_slug()
-    {
-        $slug   = 'this-is-a-test-for-limit-slug-length';
-        $length = 13;
-
-        $this->assertLessThan($length, strlen(wprs_trim_slug($slug, $length)));
-    }
+    // public function test_wprs_trim_slug()
+    // {
+    //     $slug   = 'this-is-a-test-for-limit-slug-length';
+    //     $length = 13;
+    //
+    //     $this->assertLessThan($length, strlen(wprs_trim_slug($slug, $length)));
+    // }
 
 
     /**
      * 测试文章别名转换
      */
-    public function test_post_slug_convert()
-    {
-        $post           = get_post($this->post_id);
-        $slug_converted = wprs_slug_convert($post->post_title);
-
-        $this->assertEquals($post->post_name, $slug_converted);
-    }
+    // public function test_post_slug_convert()
+    // {
+    //     $post           = get_post($this->post_id);
+    //     $slug_converted = wprs_slug_convert($post->post_title);
+    //
+    //     $this->assertEquals($post->post_name, $slug_converted);
+    // }
 
 
     /**
      * 测试分类别名转换
      */
-    public function test_term_slug_convert()
-    {
-        $term           = get_term_by('id', $this->term_id, 'category');
-        $slug_converted = wprs_slug_convert($term->name);
-
-        $this->assertEquals($term->slug, $slug_converted);
-    }
-
-
+    // public function test_term_slug_convert()
+    // {
+    //     $term           = get_term_by('id', $this->term_id, 'category');
+    //     $slug_converted = wprs_slug_convert($term->name);
+    //
+    //     $this->assertEquals($term->slug, $slug_converted);
+    // }
 
     /**
      * 测试获取插件设置
      */
-    public function test_wprs_plugin_get_option()
-    {
-        $this->assertEquals(34, wprs_plugin_get_option('wprs_pinyin_slug', 'length', 34));
-    }
+    // public function test_wprs_plugin_get_option()
+    // {
+    //     $this->assertEquals(34, wprs_plugin_get_option('wprs_pinyin_slug', 'length', 34));
+    // }
 
 }
