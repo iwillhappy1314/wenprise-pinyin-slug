29391b5e260a0bf10353c9a6a6a9244d8af91d91
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2020-04-02 19:55:13 +0800

    完善测试

--- wenprise-pinyin-slug.php
+++ wenprise-pinyin-slug.php
@@ -13,28 +13,36 @@ Requires PHP: 5.6
 
 define('WPRS_PS_PATH', plugin_dir_path(__FILE__));
 
-if (PHP_VERSION_ID < 50600) {
+add_action('plugins_loaded', function ()
+{
+    if (PHP_VERSION_ID < 50600) {
 
-    // 显示警告信息
-    if (is_admin()) {
-        add_action('admin_notices', function ()
-        {
-            printf('<div class="error"><p>' . __('Wenprise Pinyin Slug 需要 PHP %1$s 以上版本才能运行，您当前的 PHP 版本为 %2$s， 请升级到 PHP 到 %1$s 或更新的版本， 否则插件没有任何作用。',
-                    'wprs') . '</p></div>',
-                '5.6.0', PHP_VERSION);
-        });
+        // 显示警告信息
+        if (is_admin()) {
+            add_action('admin_notices', function ()
+            {
+                printf('<div class="error"><p>' . __('Wenprise Pinyin Slug 需要 PHP %1$s 以上版本才能运行，您当前的 PHP 版本为 %2$s， 请升级到 PHP 到 %1$s 或更新的版本， 否则插件没有任何作用。',
+                        'wprs') . '</p></div>',
+                    '5.6.0', PHP_VERSION);
+            });
+        }
+
+        return;
     }
 
-    return;
-}
+    /**
+     * 插件插件设置链接
+     */
+    add_filter('plugin_action_links_' . plugin_basename(__FILE__), function ($links)
+    {
+        $url = admin_url('options-general.php?page=wenprise_pinyin_slug');
+        $url = '<a href="' . esc_url($url) . '">' . __('设置') . '</a>';
+        array_unshift($links, $url);
 
-add_filter('plugin_action_links_' . plugin_basename(__FILE__), function ($links)
-{
-    $url = admin_url('options-general.php?page=wenprise_pinyin_slug');
-    $url = '<a href="' . esc_url($url) . '">' . __('设置') . '</a>';
-    array_unshift($links, $url);
+        return $links;
+    });
 
-    return $links;
+
+    // 加载插件主要功能
+    require_once(plugin_dir_path(__FILE__) . 'vendor/autoload.php');
 });
-
-require_once(plugin_dir_path(__FILE__) . 'vendor/autoload.php');
